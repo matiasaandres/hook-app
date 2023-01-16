f1202dc8b6eddf332bebdbd48bd80d9aac11e593
Matias Morales <matias.andres.mh@gmail.com>
2023-01-16 17:36:57 -0300

    useCounter custonHook

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const CounterApp = () => {
 
 
-    const [counter,setCounter] = useState(10);
 
+  const [state, setCounter] = useState({
+    counter1:10,
+    counter2:20,
+    counter3:30
+  });
+
+  const {counter1, counter2, counter3} = state;
 
   return (
-   
-<>
-    <h1>Counter: {counter}</h1>
+    <>
+      <h1>Counter 1: {counter1}</h1>
+      <h1>Counter 2: {counter2}</h1>
+      <h1>Counter 3: {counter3}</h1>
 
-    
 
-    <hr />
-  
-    <button onClick={()=> setCounter(counter+1)}>+1</button>
+      <hr />
 
+      <button onClick={() => setCounter({
+                    ...state,
+                    counter1:counter1+1
+                 
+      })}>+1</button>
     </>
   );
-}
+};
